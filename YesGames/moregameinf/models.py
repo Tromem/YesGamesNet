638786ba5e138b0fbab00f5021f9f490a162b49e
Tromem <denisdyb123@gmail.com>
2025-03-27 21:21:15 +1000

    Добавлен класс с товаром, с базовыми полями

--- YesGames/moregameinf/models.py
+++ YesGames/moregameinf/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import Buyer
 
-# Create your models here.
+class Goods():
+
+    GoodsName = models.TextField(verbose_name="название", max_length=40)
+    prise = models.IntegerField(verbose_name='Цена')
+    description = models.TextField(verbose_name='описание',max_length=1000)
+    images = models.ImageField('фото товара',upload_to='media/goods')
+    byUser = models.ForeignKey(Buyer , on_delete=models.CASCADE)
+
+

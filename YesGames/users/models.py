14fd904eb2e3e18c2d1e7ee4a9564c9c62a2d2d6
Tromem <denisdyb123@gmail.com>
2025-03-27 21:19:03 +1000

    Добавлены 2 класса в модели , Покупателя и коментарии

--- YesGames/users/models.py
+++ YesGames/users/models.py
@@ -1,7 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.forms import ModelForm
+
+
+
 
 class Buyer(AbstractUser):
+    
+    description = models.TextField(verbose_name='Описание продавца',name='overviev',max_length='1000' ,blank=True)
+    userpic = models.ImageField(upload_to='media', blank=True,verbose_name='фото')
 
-    pass
+ 
+class Comment(models.Model):
+    object_id = models.PositiveIntegerField('ИД поста', default=1)
+    Fromuser = models.ForeignKey(Buyer , 
+                                 on_delete=models.CASCADE,
+                                 related_name='comment_fromUser')
+    
+    recepiend = models.ForeignKey(Buyer , 
+                                  on_delete=models.CASCADE,
+                                  related_name="comments_received",)
+    
+    text = models.TextField(verbose_name='комментарий', max_length=200)
+    created = models.DateTimeField(default=timezone.now )
+    
+    def __str__(self):
+        return self.text
+        
+       
     

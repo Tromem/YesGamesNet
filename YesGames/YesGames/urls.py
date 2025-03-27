441fd1c850bde088c70a4fc52d1948c80b3dcef8
Tromem <denisdyb123@gmail.com>
2025-03-27 21:21:50 +1000

    отслеживание юрлок в мейне

--- YesGames/YesGames/urls.py
+++ YesGames/YesGames/urls.py
@@ -4,10 +4,13 @@ from django.contrib import admin
 from main import urls as main
 from users import urls as users
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main)),
     path('accounts/', include("django.contrib.auth.urls")),
     path("users/", include(users))
-]
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
